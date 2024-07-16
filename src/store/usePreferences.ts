ce211608f36b5a56a6e5474549297d5dcab7737a
Nicolas Davenne <nico.dvne@gmail.com>
2024-07-16 23:38:15 +0200

    Add Preferences on homepage with sounds effects

---
+++ src/store/usePreferences.ts
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import {create} from "zustand"
+
+type Preferences = {
+    soundEnabled: boolean;
+    setSoundEnabled: (soundEnabled: boolean) => void;
+}  
+
+export const usePreferences = create<Preferences>((set) => ({
+    soundEnabled: true,
+    setSoundEnabled: (soundEnabled: boolean) => set({soundEnabled})
+}));
